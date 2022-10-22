4b0d0b3c58a7859ed89a8560811382b1808d1f70
Nick Paz <nicholasmpaz@gmail.com>
2022-10-22 13:37:30 -0700

    Implement swaps by moving editors

--- src/layouts/baseLayout.ts
+++ src/layouts/baseLayout.ts
@@ -1,9 +1,42 @@
 import * as vscode from "vscode";
+import { extensionNamespace } from "../definitions/constants";
 import { maxEditorGroups } from "./definitions/constants";
-import { VscodeCommand } from "./definitions/types";
+import {
+  GlobalStateKey,
+  Orientation,
+  VscodeCommand,
+} from "./definitions/types";
+
+// moveActiveEditor - Move the active editor by tabs or groups
+
+// Active editor move argument - Argument Properties:
+// 'to': String value providing where to move.
+// 'by': String value providing the unit for move (by tab or by group).
+// 'value': Number value providing how many positions or an absolute position to move.
 
 abstract class BaseLayout {
+  context: vscode.ExtensionContext;
+
   public abstract setLayout(): Promise<void>;
+
+  public constructor(context: vscode.ExtensionContext) {
+    this.context = context;
+  }
+
+  public getCommandLock(): number {
+    return (
+      this.context.globalState.get(
+        `${extensionNamespace}.${GlobalStateKey.commandLock}`
+      ) ?? 0
+    );
+  }
+
+  public setCommandLock(time: number): Thenable<void> {
+    return this.context.globalState.update(
+      `${extensionNamespace}.${GlobalStateKey.commandLock}`,
+      time
+    );
+  }
 
   public async newEditorGroup() {
     const { all: tabGroups } = vscode.window.tabGroups;
@@ -30,55 +63,55 @@ abstract class BaseLayout {
   }
 
   public async swapNextEditorGroup() {
-    const { document: activeDocument, viewColumn: activeViewColumn } =
-      vscode.window.activeTextEditor || {};
-    await vscode.commands.executeCommand(VscodeCommand.closeActiveEditor);
-    await vscode.commands.executeCommand(VscodeCommand.focusNextEditorGroup);
-    const { document: swappedDocument, viewColumn: swappedViewColumn } =
-      vscode.window.activeTextEditor || {};
-    await vscode.commands.executeCommand(VscodeCommand.closeActiveEditor);
+    const { all: tabGroups } = vscode.window.tabGroups;
 
-    if (
-      !activeViewColumn ||
-      !activeDocument ||
-      !swappedViewColumn ||
-      !swappedDocument
-    ) {
+    if (tabGroups.length <= 1) {
       return;
     }
-    await vscode.window.showTextDocument(
-      swappedDocument,
-      activeViewColumn,
-      true
+    const { viewColumn } = vscode.window.activeTextEditor ?? {};
+    const isLastEditorGroup = tabGroups.length === viewColumn;
+
+    await vscode.commands.executeCommand(
+      isLastEditorGroup
+        ? VscodeCommand.moveEditorToFirstGroup
+        : VscodeCommand.moveEditorToNextGroup
     );
-    await vscode.window.showTextDocument(activeDocument, swappedViewColumn);
+    await vscode.commands.executeCommand(
+      VscodeCommand.openPreviousEditorInGroup
+    );
+    await vscode.commands.executeCommand(
+      isLastEditorGroup
+        ? VscodeCommand.moveEditorToLastGroup
+        : VscodeCommand.moveEditorToPreviousGroup
+    );
+    await vscode.commands.executeCommand(VscodeCommand.focusNextEditorGroup);
   }
 
   public async swapPreviousEditorGroup() {
-    const { document: activeDocument, viewColumn: activeViewColumn } =
-      vscode.window.activeTextEditor || {};
-    await vscode.commands.executeCommand(VscodeCommand.closeActiveEditor);
+    const { all: tabGroups } = vscode.window.tabGroups;
+
+    if (tabGroups.length <= 1) {
+      return;
+    }
+    const { viewColumn } = vscode.window.activeTextEditor ?? {};
+    const isFirstEditorGroup = viewColumn === 1;
+
+    await vscode.commands.executeCommand(
+      isFirstEditorGroup
+        ? VscodeCommand.moveEditorToLastGroup
+        : VscodeCommand.moveEditorToPreviousGroup
+    );
+    await vscode.commands.executeCommand(
+      VscodeCommand.openPreviousEditorInGroup
+    );
+    await vscode.commands.executeCommand(
+      isFirstEditorGroup
+        ? VscodeCommand.moveEditorToFirstGroup
+        : VscodeCommand.moveEditorToNextGroup
+    );
     await vscode.commands.executeCommand(
       VscodeCommand.focusPreviousEditorGroup
     );
-    const { document: swappedDocument, viewColumn: swappedViewColumn } =
-      vscode.window.activeTextEditor || {};
-    await vscode.commands.executeCommand(VscodeCommand.closeActiveEditor);
-
-    if (
-      !activeViewColumn ||
-      !activeDocument ||
-      !swappedViewColumn ||
-      !swappedDocument
-    ) {
-      return;
-    }
-    await vscode.window.showTextDocument(
-      swappedDocument,
-      activeViewColumn,
-      true
-    );
-    await vscode.window.showTextDocument(activeDocument, swappedViewColumn);
   }
 
   public async toggleEditorGroupSpotlight() {
